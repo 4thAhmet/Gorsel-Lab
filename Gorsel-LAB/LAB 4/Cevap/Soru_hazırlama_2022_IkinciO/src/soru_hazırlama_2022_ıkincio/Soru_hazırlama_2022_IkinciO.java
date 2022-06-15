973a92467464f6f6141e94b658ef1c7fda33e065
4thAhmet <ahmetaakkeci@gmail.com>
2022-06-15 21:08:58 +0300

    2022-Gorsel
    2022 Gorsel Programlama (iö) Tüm Lab Soruları

---
+++ Gorsel-LAB/LAB 4/Cevap/Soru_hazırlama_2022_IkinciO/src/soru_hazırlama_2022_ıkincio/Soru_hazırlama_2022_IkinciO.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package soru_hazırlama_2022_ıkincio;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class Soru_hazırlama_2022_IkinciO {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
---
+++ Gorsel-LAB/LAB 8/Gorsel_Lab_8_NO/src/gorsel_lab_8_no/Gorsel_Lab_8_NO.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gorsel_lab_8_no;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class Gorsel_Lab_8_NO {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
